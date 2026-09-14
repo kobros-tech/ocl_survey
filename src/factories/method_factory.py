eeeb7e6b698370023dc5fb4913ed4516c62e0648
kobros-tech <alkobroslymohamed@gmail.com>
2026-09-14 07:23:44 +0200

    class oracle is perfect, try prope and compare

--- src/factories/method_factory.py
+++ src/factories/method_factory.py
@@ -179,7 +179,7 @@ def create_strategy(
                 strategy_kwargs.get("reuse_is_mutable", True)
             ),
             force_decision=strategy_kwargs.get("force_decision", None),
-            eval_routing=strategy_kwargs.get("eval_routing", "class_oracle"),
+            eval_routing=strategy_kwargs.get("eval_routing", "probe"),
             verbose=bool(strategy_kwargs.get("verbose", True)),
         )
         plugins.append(skill_plugin)
